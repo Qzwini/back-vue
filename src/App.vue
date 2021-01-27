9f76e08b4a3bbe804e63ee4d9d2a602b3967fae2
qzwini <qzwini@gmail.com>
2021-01-27 20:29:03 +0300

    hi

--- src/App.vue
+++ src/App.vue
@@ -1,18 +1,55 @@
 <template>
-  <img alt="Vue logo" src="./assets/logo.png">
-  <HelloWorld msg="Welcome to Your Vue.js App"/>
+<div>
+  
+<h1>{{title}}</h1><br>
+<div v-if="showModal">
+<Modal :header="hrader" :text="text" :theam="sale" @close='toggalModal'>
+<h2>Hi ali</h2>
+<p>Lol Fuck u ...</p>
+<hr>
+<template v-slot:links>
+  <a href="#">sign up now</a>
+  <a href="#">more info</a>
+</template>
+</Modal>
+</div>
+<input type="text" ref="name">
+<button @click="handleClick">click me</button>
+<br><br><button @click.alt="toggalModal">Open modal (alt)</button><br>
+
+
+</div>
 </template>
 
-<script>
-import HelloWorld from './components/HelloWorld.vue'
 
+<script>
+import Modal from './components/Modal.vue'
 export default {
   name: 'App',
-  components: {
-    HelloWorld
-  }
+  data(){
+    return{
+      title: 'My first application vuejs ;)',
+      hrader: 'Sign up for the Giveaway!',
+      text: 'Grab your ninja swag for half price!',
+      sale: 'salee',
+      showModal: false,
+  }},
+  methods:{
+    handleClick(){
+      console.log(this.$refs.name)
+      this.$refs.name.classList.add('active')
+      this.$refs.name.focus()
+    },
+    toggalModal(){
+      this.showModal=!this.showModal
+    }
+  },
+  components: { Modal }
+ 
 }
 </script>
+
+
 
 <style>
 #app {
@@ -22,5 +59,10 @@ export default {
   text-align: center;
   color: #2c3e50;
   margin-top: 60px;
+}
+h1{
+  border-bottom: 1px solid #ddd;
+  display: inline-block;
+  padding-bottom: 10px;
 }
 </style>
---
+++ src/components/Modal.vue
@@ -0,0 +1,95 @@
+<template>
+  <div class="backdrop" @click.self="closeModal">
+      <div class="modal"  :class="{sale: theam == 'sale'}">
+          <h1>{{header}}</h1>
+          <p>{{text}}</p>
+          <slot>defolt</slot>
+          <div class="actions">
+          <a href="">it's me</a>
+              <slot name="links" class="a"></slot>
+          <a href="">it's me</a>
+
+            </div>
+      </div>
+      </div>
+
+</template>
+
+<script>
+export default {
+props: ['header', 'text', 'theam'],
+methods:{
+    closeModal(){
+        this.$emit('close')
+    }
+}
+}
+</script>
+
+<style scoped>
+.modal {
+width: 400px;
+padding: 20px;
+margin: 100px auto;
+background: white;
+border-radius: 15px;
+}
+
+.backdrop {
+    top: 0;
+    position: fixed;
+    background: rgba(0, 0, 0, .5);
+    width: 100%;
+    height:100%;
+}
+
+h1{
+    color: #03cfb4;
+    border: none;
+    padding: 0;
+}
+
+.modal .actions{
+    text-align: left;
+    margin: 40px, 0, 20px, 0;
+}
+
+.modal .actions a{
+    color: black;
+    padding: 8px;
+    border: 1px solid #eee;
+    border-radius: 4px;
+    text-decoration: none;
+    margin: 10px;
+}
+
+.modal .actions slot{
+    color: black;
+    padding: 8px;
+    border: 1px solid #eee;
+    border-radius: 4px;
+    text-decoration: none;
+    margin: 10px;
+}
+
+
+.modal.sale{
+background: crimson;
+color: white;
+}
+
+.modal.sale h1{
+    color: black;
+}
+
+.modal.sale .actions{
+    color: white;
+}
+.modal.sale .actions a{
+    color: white;
+}
+
+
+
+
+</style>
